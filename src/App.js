881d7a829fc855cbe253aef5a3836cf2eade0b7e
Russell Passmore <russellpassmore2005@gmail.com>
2021-09-24 20:14:10 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataObj: null,
+            loaded: true
+        }
+    }
+
+
+    async componentDidMount(){
+        const url = `https://api.nasa.gov/planetary/apod?api_key=4yGlazBb3PftsRRldGwC15JNnjayMwNxE4pEPc1P`;
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({dataObj: data});
+        console.log(this.state.dataObj.copyright)
+    }
+
+    render() {
+        return(
+            <div>
+                {this.state.loaded || !this.state.dataObj ? (
+                    <h1>loading...</h1>
+                ) : (
+                    <h1>{this.state.dataObj.copyright}</h1>
+                )}
+            </div>
+        );
+    }
 }
-
-export default App;
