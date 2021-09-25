bc39168f8cad12745e0d7fd3fbf6aebd5615d666
Russell Passmore <russellpassmore2005@gmail.com>
2021-09-25 02:07:06 -0400

    added really cool card thing

--- src/App.js
+++ src/App.js
@@ -52,9 +52,10 @@ export default class App extends React.Component{
                     </div>
                 ) : (
                     <div className="mainContainer">
-                        <h1 className="photoName">{this.state.dataObj.copyright}</h1>
+                        <h1 className="photoName">{this.state.dataObj.title}</h1>
                         <h3 className="timeFormat">Taken from NASA APOD at {this.state.time}</h3>
                         <div className="infoCard">
+                            <h2>{this.state.dataObj.copyright}</h2>
                             <p className="explanationText"> {this.state.dataObj.explanation}</p>
                                 <img src={this.state.dataObj.hdurl} className="apodImg"/>
                         </div>
