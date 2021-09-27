014f0c7fd760cec5d02a48804432004568738a0d
MetallicGoat <the.great.metallic.goat@gmail.com>
2021-09-27 18:35:32 -0400

    Organize & Add some comments

--- src/App.js
+++ src/App.js
@@ -14,11 +14,20 @@ export default class App extends React.Component{
             sol: 0,
             SolInput: ''
         }
+
+        // Load defaults
         this.changeImgIndex = this.changeImgIndex.bind(this);
         this.changeInput = this.changeInput.bind(this);
         this.changeSol = this.changeSol.bind(this);
     }
 
+    //Load Defaults
+    componentDidMount(){
+        this.fetchApodData();
+        this.fetchSol(this.state.sol);
+    }
+
+    // Returns a the date
     getDate() {
         let today = new Date();
         let hours = String(today.getHours());
@@ -36,18 +45,15 @@ export default class App extends React.Component{
         })
     }
 
+    // Change roverPhotoIndex to a random new one
     changeImgIndex(){
         this.setState({
             roverPhotoIndex: Math.floor(Math.random()*this.state.photoArrLength)
         })
     }
 
-    componentDidMount(){
-        this.fetchData();
-        this.fetchSol(this.state.sol);
-    }
-
-    async fetchData(){
+    // Pull JSON from NASA's APDO API
+    async fetchApodData(){
         const apod = await fetch(`https://api.nasa.gov/planetary/apod?api_key=4yGlazBb3PftsRRldGwC15JNnjayMwNxE4pEPc1P`);
         const apodData = await apod.json();
         window.setInterval(()=>{
@@ -58,6 +64,7 @@ export default class App extends React.Component{
         this.getDate();
     }
 
+    // Pull JSON from NASA's Curiosity Photos API
     async fetchSol (inputSol){
         const rover = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${inputSol}&api_key=4yGlazBb3PftsRRldGwC15JNnjayMwNxE4pEPc1P`);
         const roverData = await rover.json();
@@ -91,15 +98,24 @@ export default class App extends React.Component{
                     </div>
                 ) : (
                     <div className="mainContainer">
+
+                        {/* Title/Introduction */}
+
                         <h1>NASA API WEBSITE</h1>
                         <p>Here you can see the Astronomy Picture of the Day that updates everyday around midnight, as well as the curiosity rovers photos.</p>
                         <h3 className="timeFormat">Taken from NASA APOD at {this.state.time}</h3>
+
+                        {/* Card 1 (APOD) */}
+
                         <div className="infoCard">
                             <h1 className="photoName">{this.state.apodData.title}</h1>
                             <h2>{this.state.apodData.copyright}</h2>
                             <p className="explanationText"> {this.state.apodData.explanation}</p>
                                 <img src={this.state.apodData.hdurl} className="apodImg"/>
                         </div>
+
+                        {/* Card 2 (Curiosity) */}
+
                         {!this.state.roverData ? <p> </p> : (
                             <div className="infoCard rover">
                                 <h2>Photos are from Sol {this.state.sol}</h2>
