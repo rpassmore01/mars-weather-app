c5e85f4a3e51caf9b17af288ad7526b92cf96ce0
Russell Passmore <russellpassmore2005@gmail.com>
2021-09-26 20:12:03 -0400

    Now you get images from a random sol not just the same one, with a backup for if sol has no photos

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ export default class App extends React.Component{
             roverData: null,
             time: '',
             roverPhotoIndex: 0,
-            photoArrLength: 0
+            photoArrLength: 0,
+            sol: 0
         }
         this.changeImgIndex = this.changeImgIndex.bind(this);
     }
@@ -39,16 +40,18 @@ export default class App extends React.Component{
     }
 
     async componentDidMount(){
+        const randomSol = Math.round(Math.floor(Math.random()*1000))
         const apod = await fetch(`https://api.nasa.gov/planetary/apod?api_key=4yGlazBb3PftsRRldGwC15JNnjayMwNxE4pEPc1P`);
         const apodData = await apod.json();
-        const rover = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=4yGlazBb3PftsRRldGwC15JNnjayMwNxE4pEPc1P`);
+        const rover = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${randomSol}&api_key=4yGlazBb3PftsRRldGwC15JNnjayMwNxE4pEPc1P`);
         const roverData = await rover.json();
         window.setInterval(()=>{
             this.setState({
                 apodData: apodData,
                 roverData: roverData,
-                photoArrLength: roverData.photos.length
-            });
+                photoArrLength: roverData.photos.length,
+                sol: randomSol
+            },)
         }, 3000)
         this.getDate();
     }
@@ -76,9 +79,15 @@ export default class App extends React.Component{
                         </div>
                         {!this.state.roverData ? <p> </p> : (
                             <div className="infoCard rover">
-                                <h2>Image ID: {this.state.roverData.photos[this.state.roverPhotoIndex].id}</h2>
-                                <p>Browse {this.state.photoArrLength} from Nasa's Mars Curiosity Rover!</p>
-                                <img src={this.state.roverData.photos[this.state.roverPhotoIndex].img_src} className="apodImg"/>
+                                <h2>Photos are from Sol {this.state.sol}</h2>
+                                {this.state.roverData.photos.length === 0 ? (
+                                    <h3>No Images On This Day!</h3>
+                                ): (
+                                    <div className="onLoad">
+                                        <p>Browse {this.state.photoArrLength} photos from Nasa's Mars Curiosity Rover!</p>
+                                        <img src={this.state.roverData.photos[this.state.roverPhotoIndex].img_src} className="apodImg"/>
+                                    </div>
+                                )}
                                 <button className="roverBtn" onClick={this.changeImgIndex} >Change Photo!</button>
                             </div>)}
                     </div>
